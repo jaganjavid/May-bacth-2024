9d74165735960d6788786e26b68cf425bb969e56
jaganjavid <jagantutor1@gmail.com>
2024-07-09 20:58:43 +0530

    added a storage

--- 2-javascript/6-object-project/app.js
+++ 2-javascript/6-object-project/app.js
@@ -28,7 +28,7 @@ function UI() {
              <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
-            <td>
+            <td class="delete">
                 <span class="btn btn-danger">X</span>
             </td>
         `;
@@ -46,7 +46,45 @@ function UI() {
     UI.prototype.clearTasks = function(){
         document.querySelector("#book-list").innerHTML = "";
     }
+
+    UI.prototype.deleteBook = function(item){
+       item.parentElement.remove();
+    }
+
+    UI.prototype.showAlert = function(message, type){
+
+        this.clearAlert();
+
+        // Create a div
+        const div = document.createElement("div");
+
+        // Add Class
+        div.className = `alert alert-${type}`;
+
+        // Add Text 
+        div.innerText = message;
+
+        // Get a show alert parent
+        document.querySelector(".show-alert").appendChild(div);
+
+        setTimeout(function(){
+            document.querySelector(".alert").remove();
+            this.clearAlert();
+        }, 3000);
+
+    }
+    
+    UI.prototype.clearAlert = function(){
+
+        const currentAlert = document.querySelector(".alert");
+
+        if(currentAlert){
+            currentAlert.remove();
+        }
+
+    }
 }
+
 
 // Stroage
 
@@ -68,17 +106,22 @@ document.querySelector("#book-form").addEventListener("submit", function (e) {
     const author = document.querySelector("#author").value;
     const isbn = document.querySelector("#isbn").value;
 
+  
+    const ui = new UI();
+
     // Validate 
 
     if (title === "" || author === "" || isbn === "") {
-        alert("Please fill all the fields");
-    } else {
 
-        const ui = new UI();
+        ui.showAlert("Please fill all the fields", "danger");
+
+    } else {
 
         const book = new Book(title, author, isbn);
 
         ui.addBookToList(book);
+
+        ui.showAlert("Book Added", "success");
 
         ui.clearFields();
 
@@ -86,6 +129,23 @@ document.querySelector("#book-form").addEventListener("submit", function (e) {
 
 
 });
+
+
+// Delete Task
+document.querySelector("#book-list").addEventListener("click", function(e){
+
+    if(e.target.parentElement.classList.contains("delete")){
+        
+        const ui = new UI();
+
+        // Delete Book
+        ui.deleteBook(e.target.parentElement);
+        ui.showAlert("Book Deleted", "success");
+
+
+    }
+
+})
 
 
 // Clear All
@@ -96,21 +156,47 @@ document.querySelector("#clear").addEventListener("click", function(e){
 
     ui.clearTasks();
 
+    ui.showAlert("All book clear", "success");
+
 
 })
 
 
 
-const arr = [1,2,3];
 
-const x = arr.forEach(function(num){
-   console.log(num);
-});
 
-console.log(x);
 
-const y = arr.map(function(num){
-    return num;
-});
 
-console.log(y);
+
+
+
+
+
+// setTimeout(function(){
+//     console.log("Hello");
+// }, 5000)
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = [1,2,3];
+
+// const x = arr.forEach(function(num){
+//    console.log(num);
+// });
+
+// console.log(x);
+
+// const y = arr.map(function(num){
+//     return num;
+// });
+
+// console.log(y);
